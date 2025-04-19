d530c1a32dd7a735ff9a06c4f58859ed1c884bd7
Dev Patel <devpatel@socialpilot.co>
2025-04-19 19:17:44 +0530

    refactor: remove outdated TODO comment regarding variable naming in retrieveData APIs

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 // TODO: add security in submit answer api
 //TODO: currently it is compulsory to write {} in find({}), in short mandatory to put empty object, fix that
-// TODO: there is variables names like question, answer in retrieveData apis, it should have names like questionRedisKey, answerRedisKey
 import http from 'http'
 import app from './app'
 import config from 'config'
