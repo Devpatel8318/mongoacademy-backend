a97c8b41bc29b56cea519067b27695e78850ae7d
Dev Patel <devpatel@socialpilot.co>
2025-05-16 12:13:06 +0530

    feat: add manual deployment script and enable workflow dispatch in deploy.yml

---
+++ src/scripts/manualDeploy.mjs
@@ -0,0 +1,35 @@
+/* global console */
+import axios from 'axios'
+import process from 'process'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const OWNER = 'Devpatel8318'
+const REPO = 'mongoacademy-backend'
+const WORKFLOW_FILE_NAME = 'deploy.yml'
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+
+const REF = 'main' // branch to run workflow on
+
+const triggerWorkflow = async () => {
+	try {
+		await axios.post(
+			`https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows/${WORKFLOW_FILE_NAME}/dispatches`,
+			{
+				ref: REF,
+			},
+			{
+				headers: {
+					Accept: 'application/vnd.github+json',
+					Authorization: `Bearer ${GITHUB_TOKEN}`,
+					'X-GitHub-Api-Version': '2022-11-28',
+				},
+			}
+		)
+		console.log('Workflow triggered successfully!')
+	} catch (error) {
+		console.error('Failed to trigger workflow:', error)
+	}
+}
+
+triggerWorkflow()
