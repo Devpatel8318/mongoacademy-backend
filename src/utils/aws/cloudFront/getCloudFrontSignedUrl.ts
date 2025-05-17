421203599f2a8b8394d0b7e57e5db61c774d9ddc
Dev Patel <devpatel@socialpilot.co>
2025-05-17 11:40:13 +0530

    changed log

--- src/utils/aws/cloudFront/getCloudFrontSignedUrl.ts
+++ src/utils/aws/cloudFront/getCloudFrontSignedUrl.ts
@@ -4,7 +4,7 @@ import config from 'config'
 const getCloudFrontSignedUrl = (s3ObjectKey: string) => {
 	const url = `${config.aws.cloudFront.cloudFrontUrl}/${s3ObjectKey}`
 
-	console.log({ url })
+	console.log('url', url)
 
 	return getSignedUrl({
 		url,
