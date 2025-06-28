97408e9a844ae22bfbf936fa7f74e7e1807e0333
Dev Patel <devpatelm8318@gmail.com>
2025-06-28 22:17:54 +0530

    feat: configure dotenv to load environment variables from specific development file

--- src/scripts/manualDeploy.mjs
+++ src/scripts/manualDeploy.mjs
@@ -2,7 +2,8 @@
 import axios from 'axios'
 import process from 'process'
 import dotenv from 'dotenv'
-dotenv.config()
+const NODE_ENV = 'development'
+dotenv.config({ path: `.env.${NODE_ENV}` })
 
 const OWNER = 'Devpatel8318'
 const REPO = 'mongoacademy-backend'
